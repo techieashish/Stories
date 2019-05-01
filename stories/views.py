38f912a876138ecc208c5ce8f236fdf6fbf4eb43
techieashish <ashishsrivastava_18@yahoo.in>
2019-05-01 16:10:22 +0530

    added user_login anf logout

--- stories/views.py
+++ stories/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
+from .forms import Login, Registration
 
 # Create your views here.
 
 def index(request):
     return render(request, "main.html")
+
+
+def user_login(request):
+    form = Login(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            login(request, user)
+            return redirect('')
+
+    return render(request, 'login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('stories:login')
+
 
