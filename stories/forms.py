8e03fe8db2d9344a99a901bb86db76fea308e923
techieashish <ashishsrivastava_18@yahoo.in>
2019-05-01 16:08:16 +0530

    added login and registration form

--- stories/forms.py
+++ stories/forms.py
@@ -2,3 +2,18 @@ __author__ = 'ASHISH'
 from django import forms
 from django.contrib.auth.admin import User
 
+class Login(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class Registration(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password'
+        ]
